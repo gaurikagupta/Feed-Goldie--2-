dbedfa3c38172ece26be3168a20ff3656664dfb5
gaurikagupta <79149108+gaurikagupta@users.noreply.github.com>
2021-05-04 19:58:53 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,24 @@
+class Fishfood{
+    constructor(foodStock){
+      this.image=loadImage("fishfood.png");
+      this.foodStock=foodStock
+    }
+  display(){
+    imageMode(CENTER);
+    image(this.image,100,300,40,40);
+    var x=510;
+    var y=260;
+    for(var i=0; i<this.foodStock; i++){
+        if(i===0){
+        x=30;
+        y=y+40;
+
+        }
+        image(this.image,x,y,40,40);
+        x=x+35;
+         }
+
+  }
+}
+
+
